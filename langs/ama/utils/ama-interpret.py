1b5da231f4070f0e6fa32bc1de54b692e5711bee
HeyTomCat <144262499+HeyTomCat@users.noreply.github.com>
2024-01-05 16:31:18 +0000

    Updated ama-interpret.py

--- langs/ama/utils/ama-interpret.py
+++ langs/ama/utils/ama-interpret.py
@@ -1,11 +1,15 @@
 import sys
 
+def error(msg):
+  print(msg)
+  sys.exit(1)
+
 def parse(program):
   #removes comments and splits up into keywords
   return [instruction for instruction in [line.split(';')[0].split() for line in program.split('\n')] if instruction != []]
 
-opcode = {
-  'push': 0x0,
+def getOpcode(instruction):
+  res = {'push': 0x0,
   'pop': 0x1,
   'add': 0x2,
   'sub': 0x3,
@@ -20,11 +24,12 @@ opcode = {
   'sfl': 0xc,
   'sfg': 0xd,
   'sfe': 0xe,
-  'ext': 0xf
-}
+  'ext': 0xf}.get(instruction, 'NaN')
+  if res == 'NaN':
+    error('Instruction not found: ' + instruction)
+  return res
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
-    print("Invalid number of arguments!")
-    sys.exit(1)
+    error("Invalid number of arguments!")
   print(parse(open(sys.argv[1], "r").read()))
